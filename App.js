5cb22562162dd94c8efa6d7bdcac84538ad4fb2c
wkwksama <mailpakgasing@gmail.com>
2019-02-16 15:22:55 +0700

    render list

--- App.js
+++ App.js
@@ -3,25 +3,55 @@ import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import MovieCard from './components/MovieCard'
 
 export default class App extends React.Component {
+  state = {
+    movies: [
+      {
+        id: 1,
+        title: 'Terlalu Tampan',
+        origin: 'Indonesia',
+        rate: 7
+      },
+      {
+        id: 2,
+        title: 'Dragon Ball Z',
+        origin: 'Japan',
+        rate: 9
+      },
+      {
+        id: 3,
+        title: 'Alita Battle Angel',
+        origin: 'US',
+        rate: 9
+      },
+      {
+        id: 4,
+        title: 'Si Doel',
+        origin: 'Indonesia',
+        rate: 9
+      },
+      {
+        id: 5,
+        title: 'React',
+        origin: 'Semarang',
+        rate: 9
+      },
+    ]
+  }
+
   render() {
     return (
       <View style={styles.main}>
-        <MovieCard
-          title="Terlalu Tampan Z"
-          origin="Indonesia"
-        />
-        <MovieCard
-          title="Creed 2"
-          origin="US"
-        />
-        <MovieCard
-          title="Alita battle Angel"
-          origin="US"
-        />
-        <MovieCard
-          title="Simba"
-          origin="other"
-        />
+        {
+          this.state.movies.map(function(movie) {
+            return (
+              <MovieCard
+                key={movie.id}
+                title={movie.title}
+                origin={movie.origin}
+              />
+            )
+          })
+        }
       </View>
     );
   }
